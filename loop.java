94d0cd8d8535ad1c33b07408af9ec602ac2b3396
SYLVESTER DARIUS <silverstar.381@icloud.com>
2026-05-03 13:05:43 +0300

    Add LoopExample class with a simple for loop

---
+++ loop.java
@@ -0,0 +1,10 @@
+public class LoopExample {
+    public static void main(String[] args) {
+
+        // Loop from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+
+    }
+}
